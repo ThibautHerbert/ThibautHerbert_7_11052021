32dba9dfb52a006358d025e2c7ea7ef906dce020
Thibaut <herbert.thibaut@protonmail.com>
2021-05-14 08:39:20 -0400

    prep routes + test sql

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
 const app = express()
-//const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 require('dotenv').config()
 
@@ -20,15 +20,16 @@ const pool = mysql.createPool({
 
 });
 
-// routes : 
-    // Get All routes from employees table
+// all routes :
+    // routes Users
+        // Get All routes from employees table
 app.get('', (req, res) => {
     pool.getConnection((err, connection) => {
         if(err) throw err
         console.log(`connected as id ${ connection.threadId }`) // vérifier l'intérêt ?
 
         // query(sqlString, callback)
-        connection.query('SELECT * FROM employees', (err, rows) => {
+        connection.query('SELECT * FROM Users', (err, rows) => {
             connection.release() // return the connection to pool
 
             if(!err) {
@@ -39,14 +40,14 @@ app.get('', (req, res) => {
         })
     })
 });
-    // get an account/employee by Id
+        // get an account/employee by Id
 app.get('/:id', (req, res) => {
     pool.getConnection((err, connection) => {
         if(err) throw err
         console.log(`connected as id ${ connection.threadId }`) // vérifier l'intérêt ?
 
         // query(sqlString, callback)
-        connection.query('SELECT * FROM employees WHERE idEmployee= ?', [req.params.idEmployee], (err, rows) => { // ? is a placeholder ; [req] use the bodyparser
+        connection.query('SELECT * FROM Users WHERE id= ?', [req.params.idEmployee], (err, rows) => { // ? is a placeholder ; [req] use the bodyparser
             connection.release() // return the connection to pool
 
             if(!err) {
@@ -57,14 +58,15 @@ app.get('/:id', (req, res) => {
         })
     })
 });
-    // delete an account/employee by Id
+/*
+        // delete an account/employee by Id
 app.delete('/:id', (req, res) => {
     pool.getConnection((err, connection) => {
         if(err) throw err
         console.log(`connected as id ${ connection.threadId }`) // vérifier l'intérêt ?
 
         // query(sqlString, callback)
-        connection.query('DELETE FROM employees WHERE idEmployee= ?', [req.params.idEmployee], (err, rows) => { // ? is a placeholder ; [req] use the bodyparser
+        connection.query('DELETE FROM employees WHERE id= ?', [req.params.idEmployee], (err, rows) => { // ? is a placeholder ; [req] use the bodyparser
             connection.release() // return the connection to pool
 
             if(!err) {
@@ -75,25 +77,30 @@ app.delete('/:id', (req, res) => {
         })
     })
 });
-    // create an account/employee
-    // à vérifier pour le post !! 
+        // create an account/employee
+        // à vérifier pour le post !! 
     app.post('/:id', (req, res) => {
         pool.getConnection((err, connection) => {
             if(err) throw err
             console.log(`connected as id ${ connection.threadId }`) // vérifier l'intérêt ?
-    
+            
+            const params = req.body
+
             // query(sqlString, callback)
-            connection.query('CREATE FROM employees WHERE idEmployee= ?', [req.params.idEmployee], (err, rows) => { // ? is a placeholder ; [req] use the bodyparser
+            connection.query('INSERT INTO Users SET ?', params , (err, rows) => { // ? is a placeholder ; [req] use the bodyparser
                 connection.release() // return the connection to pool
     
                 if(!err) {
-                    res.send(`Le compte de ${ [req.params.firstName] } ${ [req.params.lastName] } a été créé`)
+                    res.send(`Le compte de ${ params.firstName } ${ params.lastName } a été créé`)
                 } else {
                     console.log(err)
                 }
             })
         })
-    });  
+    });
+    // routes Posts
+
+    // routes Comments
     // create a message FROM messages table
         // doit récupèrer idMessage + idEmployee + comment + dateMessage
     // delete a message FROM messages table
@@ -102,7 +109,7 @@ app.delete('/:id', (req, res) => {
     // delete a content FROM content table
 
     // faire un filter pour get les plus récents content et messages ?
-
-//app.listen(port, () => console.log(`Listen on port ${port}`))
+*/
+app.listen(port, () => console.log(`Listen on port ${port}`))
 
 module.exports = app;
